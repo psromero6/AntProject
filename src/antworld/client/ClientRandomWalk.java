6852e614f2a089c8988d1c209e8fca1082a3a9d9
psromero6 <promero6@unm.edu>
2014-09-02 15:29:18 -0600

    bacon

---
+++ src/antworld/client/ClientRandomWalk.java
@@ -0,0 +1,280 @@
+package antworld.client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+import antworld.data.AntAction;
+import antworld.data.AntData;
+import antworld.data.CommData;
+import antworld.data.Constants;
+import antworld.data.Direction;
+import antworld.data.NestNameEnum;
+import antworld.data.TeamNameEnum;
+import antworld.data.AntAction.AntActionType;
+import antworld.data.AntType;
+import static antworld.client.AntWorld.readImage;
+import antworld.data.NestData;
+import java.util.ArrayList;
+
+public class ClientRandomWalk
+{
+  private static final boolean DEBUG = true;
+  private static final TeamNameEnum myTeam = TeamNameEnum.Buffalograss;
+  private static final long password = 122538603443L;//Each team has been assigned a random password.
+  private ObjectInputStream inputStream = null;
+  private ObjectOutputStream outputStream = null;
+  private boolean isConnected = false;
+  private NestNameEnum myNestName = null;
+  private int centerX, centerY;
+ 
+
+  private Socket clientSocket;
+
+  private static Random random = Constants.random;
+
+  
+  
+  
+  
+  public ClientRandomWalk(String host, int portNumber)
+  {
+    System.out.println("Starting ClientRandomWalk: " + System.currentTimeMillis());
+    isConnected = false;
+    while (!isConnected)
+    {
+      isConnected = openConnection(host, portNumber);
+    }
+    CommData data = chooseNest();
+    mainGameLoop(data);
+    closeAll();
+  }
+
+  private boolean openConnection(String host, int portNumber)
+  {
+
+    try
+    {
+      clientSocket = new Socket(host, portNumber);
+    }
+    catch (UnknownHostException e)
+    {
+      System.err.println("ClientRandomWalk Error: Unknown Host " + host);
+      e.printStackTrace();
+      return false;
+    }
+    catch (IOException e)
+    {
+      System.err.println("ClientRandomWalk Error: Could not open connection to " + host + " on port " + portNumber);
+      e.printStackTrace();
+      return false;
+    }
+
+    try
+    {
+      outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+      inputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+    }
+    catch (IOException e)
+    {
+      System.err.println("ClientRandomWalk Error: Could not open i/o streams");
+      e.printStackTrace();
+      return false;
+    }
+
+    return true;
+
+  }
+
+  public void closeAll()
+  {
+    System.out.println("ClientRandomWalk.closeAll()");
+    {
+      try
+      {
+        if (outputStream != null) outputStream.close();
+        if (inputStream != null) inputStream.close();
+        clientSocket.close();
+      }
+      catch (IOException e)
+      {
+        System.err.println("ClientRandomWalk Error: Could not close");
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public CommData chooseNest()
+  {
+    while (myNestName == null)
+    {
+      try { Thread.sleep(100); } catch (InterruptedException e1) {}
+        
+      NestNameEnum requestedNest = NestNameEnum.values()[random.nextInt(NestNameEnum.SIZE)];
+      CommData data = new CommData(requestedNest, myTeam);
+      data.password = password;
+      
+      if( sendCommData(data) )
+      {
+        try
+        {
+          if (DEBUG) System.out.println("ClientRandomWalk: listening to socket....");
+          CommData recvData = (CommData) inputStream.readObject();
+          if (DEBUG) System.out.println("ClientRandomWalk: recived <<<<<<<<<"+inputStream.available()+"<...\n" + recvData);
+          
+          if (recvData.errorMsg != null)
+          {
+            System.err.println("ClientRandomWalk***ERROR***: " + recvData.errorMsg);
+            continue;
+          }
+  
+          if ((myNestName == null) && (recvData.myTeam == myTeam))
+          { myNestName = recvData.myNest;
+            centerX = recvData.nestData[myNestName.ordinal()].centerX;
+            centerY = recvData.nestData[myNestName.ordinal()].centerY;
+            System.out.println("ClientRandomWalk: !!!!!Nest Request Accepted!!!! " + myNestName);
+            return recvData;
+          }
+        }
+        catch (IOException e)
+        {
+          System.err.println("ClientRandomWalk***ERROR***: client read failed");
+          e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+          System.err.println("ClientRandomWalk***ERROR***: client sent incorect data format");
+        }
+      }
+    }
+    return null;
+  }
+    
+  public void mainGameLoop(CommData data)
+  {
+      System.out.println("mainGameLoop");
+    while (true)
+    {
+      try
+      {
+        if (DEBUG) System.out.println("ClientRandomWalk: chooseActions: " + myNestName);
+
+        chooseActionsOfAllAnts(data);
+        
+ 
+        
+        
+        
+        
+        
+        
+        CommData sendData = data.packageForSendToServer();
+        
+        //System.out.println("ClientRandomWalk: Sending>>>>>>>: " + sendData);
+        outputStream.writeObject(sendData);
+        outputStream.flush();
+        outputStream.reset();
+       
+
+        if (DEBUG) System.out.println("ClientRandomWalk: listening to socket....");
+        CommData recivedData = (CommData) inputStream.readObject();
+        if (DEBUG) System.out.println("ClientRandomWalk: received <<<<<<<<<"+inputStream.available()+"<...\n" + recivedData);
+        data = recivedData;
+  
+        
+        if ((myNestName == null) || (data.myTeam != myTeam))
+        {
+          System.err.println("ClientRandomWalk: !!!!ERROR!!!! " + myNestName);
+        }
+      }
+      catch (IOException e)
+      {
+        System.err.println("ClientRandomWalk***ERROR***: client read failed");
+        e.printStackTrace();
+        try { Thread.sleep(1000); } catch (InterruptedException e1) {}
+
+      }
+      catch (ClassNotFoundException e)
+      {
+        System.err.println("ServerToClientConnection***ERROR***: client sent incorect data format");
+        e.printStackTrace();
+        try { Thread.sleep(1000); } catch (InterruptedException e1) {}
+      }
+
+    }
+  }
+  
+  
+  private boolean sendCommData(CommData data)
+  {
+    
+    CommData sendData = data.packageForSendToServer();
+    try
+    {
+      if (DEBUG) System.out.println("ClientRandomWalk.sendCommData(" + sendData +")");
+      outputStream.writeObject(sendData);
+      outputStream.flush();
+      outputStream.reset();
+    }
+    catch (IOException e)
+    {
+      System.err.println("ClientRandomWalk***ERROR***: client read failed");
+      e.printStackTrace();
+      try { Thread.sleep(1000); } catch (InterruptedException e1) {}
+      return false;
+    }
+
+    return true;
+    
+  }
+
+  private void chooseActionsOfAllAnts(CommData commData)
+  {
+    for (AntData ant : commData.myAntList)
+    {
+      AntAction action = chooseAction(commData, ant);
+      ant.myAction = action;
+    }
+  }
+
+  private AntAction chooseAction(CommData data, AntData ant)
+  {
+    AntAction action = new AntAction(AntActionType.STASIS);
+    
+    if (ant.ticksUntilNextAction > 0) return action;
+
+    if (ant.underground)
+    {
+      action.type = AntActionType.EXIT_NEST;
+      action.x = centerX - Constants.NEST_RADIUS + random.nextInt(2 * Constants.NEST_RADIUS);
+      action.y = centerY - Constants.NEST_RADIUS + random.nextInt(2 * Constants.NEST_RADIUS);
+      return action;
+    }
+
+    action.type = AntActionType.MOVE;
+    action.direction = Direction.getRandomDir();
+
+    return action;
+  }
+  
+  
+  public void drawAnts(CommData data) throws IOException{
+  AntWorld antworld=new AntWorld(data);
+  
+  
+  }
+
+  public static void main(String[] args)
+  {
+    String serverHost = "b146-76";
+    System.out.println(args.length);
+   if (args.length > 0) serverHost = args[0];
+    
+  new ClientRandomWalk(serverHost, Constants.PORT);
+ }
+
+}
